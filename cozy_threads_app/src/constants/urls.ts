f0a097c718670b65872dc03c47d1baea40d5af8e
Cesar Guerrero <cesarguerrero@Cesars-MacBook-Pro.local>
2024-07-31 00:55:18 -0500

    fix: use aws api gateway instead of EB endpoint

--- cozy_threads_app/src/constants/urls.ts
+++ cozy_threads_app/src/constants/urls.ts
@@ -1,2 +1,2 @@
 export const DEV_API_URL = 'http://localhost:3001';
-export const PROD_API_URL = 'http://cozy-threads-env.eba-y27gkqna.us-east-2.elasticbeanstalk.com/'
+export const PROD_API_URL = 'https://4q6arzc680.execute-api.us-east-2.amazonaws.com'
