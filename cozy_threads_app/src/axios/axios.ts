d0bc3c34a295c2e4878704a0b819b60fc6c97ced
Cesar Guerrero <guerrerocesar38@gmail.com>
2024-08-02 15:36:27 -0500

    fix: use correct env api url

--- cozy_threads_app/src/axios/axios.ts
+++ cozy_threads_app/src/axios/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { DEV_API_URL, PROD_API_URL } from "../constants/urls";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_NODE_ENV !== 'dev'
+  baseURL: process.env.REACT_APP_NODE_ENV === 'dev'
     ? DEV_API_URL
     : PROD_API_URL,
 });
